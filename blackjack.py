ddb3e99cbf4881cd9cd18b7ecb38b7c26d37148e
mblobao <57967945+mblobao@users.noreply.github.com>
2020-04-30 15:44:33 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
-from decks import Player
+from numpy.random import random
+from decks import Player, Deck
 
 
 class BJPlayer(Player):
@@ -40,10 +41,15 @@ class BJPlayer(Player):
 
 
 class BlackJack:
-    ''' BlackJackClass - Main actions for the game
-    '''
+    """ BlackJackClass - Main actions for the game """
+    class Mode:
+        dark = 0
+        one_up = 1
+        dark_dealer = 2
+        one_up_dealer = 3
 
-    def __init__(self, names, players=2):
+    def __init__(self, names, players=2, mode=Mode.dark):
+        self.__mode = mode
         if players < len(names):
             players = len(names) + 1
         self.players = [BJPlayer(name=nome, npc=False) for nome in names]
@@ -51,6 +57,8 @@ class BlackJack:
             self.players.append(BJPlayer(name=f'NPC-{i}', npc=True))
         if len(self.players) > 7:
             raise Exception("No more than 7 players, please...")
+        if mode in [self.Mode.dark_dealer, self.Mode.one_up_dealer]:
+            self.players.append(BJPlayer(name="Dealer", npc=True))
         self.deck = Deck()
 
     def __enter__(self):
@@ -67,20 +75,21 @@ class BlackJack:
         while sum(len(player) for player in self.players) < 2 * len(self.players):
             for player in self.players:
                 player.get_card(self.deck.draw())
+        for player in self.players:
+            if player.name == 'Dealer' and player.is_npc:
+                while abs(player) < 17:  # For dealer type games, dealer cannot start with less than 17 points
+                    player.get_card(self.deck.draw())
 
-    def roll(self):
-        # Play a round
-        play = sum(1 for player in self.players if not player.burn)
-        result = {'asks': 0, 'non-asks': 0, 'unknown': play}
-        asks = play
-        while asks > 0:
-            play = sum(1 for player in self.players if not player.burn)
-
-            result = {'asks': 0, 'non-asks': 0, 'unknown': play}
+    def roll(self):  # Play a round
+        plays = sum(1 for player in self.players if not player.burn or not (player.name == 'Dealer' and player.is_npc))
+        result = {'asks': 0, 'non-asks': 0, 'unknown': plays}  # plays counts the number of players in the roll
+        while result['unknown'] > 0:
             for player in self.players:
+                if player.name == 'Dealer' and player.is_npc:  # Dealers only draw cards in the game start
+
                 if not player.burn:
-                    if player.isNpc():
-                        if round(random()) == 1:
+                    if player.is_npc:
+                        if round(random()) == 1:  # Modify with machine learning decision making in future version
                             result['asks'] += 1
                             result['unknown'] -= 1
                             player.get_card(self.deck.draw())
@@ -99,7 +108,7 @@ class BlackJack:
                             result['asks'] += 1
                             result['unknown'] -= 1
                             player.get_card(self.deck.draw())
-                        else: # if 'n'
+                        else:  # if 'n'
                             result['non-asks'] += 1
                             result['unknown'] -= 1
                 player.last_roll = result
